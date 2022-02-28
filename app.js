90871f3ebf16fda67dee36e26a8fe7b968228997
RizalPurwanto <rizal.purwanto.id>
2022-02-28 08:51:13 +0700

    change app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000; 
 const router = require('./routes')
 const cors = require('cors')
+console.log("INI CORS", cors)
 
 const errorHandler = require("./middlewares/errorHandler");
 app.use(cors()); //memfilter akses. jika dalam kurung kosong, semua bisa masuk
